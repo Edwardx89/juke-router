2738148218cbf324c929deafd8f169b4160c837f
Edward Goo <Edwardx89@yahoo.com>
2017-03-28 17:48:41 -0400

    added ReactClone Element

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory } from 'react-router';
+
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Router path='/' component={AppContainer} />
+  </Router>,
   document.getElementById('app')
 );
